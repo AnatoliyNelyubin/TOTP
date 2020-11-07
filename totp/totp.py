3d52560eec2b9f9ed6ecb36aa211768afde64454
Anatolii Neliubin <anatolii@google.com>
2020-11-07 17:44:37 +0500

    functions updated, requiremets extended

--- totp/totp.py
+++ totp/totp.py
@@ -1,5 +1,7 @@
 import time
 from typing import Optional
+from base58 import b58encode
+from secrets import token_bytes
 
 # totp time window in seconds
 # would go to a config in a real microservice
@@ -9,7 +11,8 @@ time_window = 15
 
 def generate_secret() -> str:
     """Returns a random 20-symbol base58 string"""
-    return "abcdeabcdeabcdeabcd" + str(int(time.time_ns()) % 9)
+    # return "abcdeabcdeabcdeabcd" + str(int(time.time_ns()) % 9)
+    return b58encode(token_bytes(20))[:20].decode()
 
 
 def generate_code(secret: str,
@@ -28,8 +31,8 @@ def generate_code(secret: str,
         seconds_since_the_epoch = int(time.time())
     seconds_since_the_epoch = int(seconds_since_the_epoch // time_window)
 
-    code = str(seconds_since_the_epoch)[-4:]
-
+    # code = str(seconds_since_the_epoch)[-4:]
+    code = str(abs(hash(secret + str(seconds_since_the_epoch))))[:4]
     return code
 
 
@@ -45,4 +48,11 @@ def check_code(secret: str, code: str,
     """
     if len(code) != 4:
         raise ValueError("TOTP code must be 4 digits string, is '{}' instead".format(code))
-    return generate_code(secret, seconds_since_the_epoch) == code
+    if len(secret) != 20:
+        raise ValueError("secret must be 20 symbols long, but it, is '{}' instead".format(len(secret)))
+    # return generate_code(secret, seconds_since_the_epoch) == code
+    if seconds_since_the_epoch is None:
+        seconds_since_the_epoch = int(time.time())
+    seconds_since_the_epoch = int(seconds_since_the_epoch // time_window)
+    check_code = str(abs(hash(secret + str(seconds_since_the_epoch))))[:4]
+    return check_code == code
